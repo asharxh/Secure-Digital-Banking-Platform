68371c221f96713e9eead409f25deabb13f8fd5d
asharxh <asharxheikh47@gmail.com>
2026-02-20 15:02:48 +0530

    commit

---
+++ src/main/java/com/ashar/securedigitalbankingplatform/SecureDigitalBankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ashar.securedigitalbankingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDigitalBankingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureDigitalBankingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashar/securedigitalbankingplatform/SecureDigitalBankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashar.securedigitalbankingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDigitalBankingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
