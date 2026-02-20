c425206975ccbddcdd1e2b360850d8b96b4aa8bb
asharxh <asharxheikh47@gmail.com>
2026-02-20 16:25:32 +0530

    get user by id

--- src/main/java/com/ashar/securedigitalbankingplatform/service/UserService.java
+++ src/main/java/com/ashar/securedigitalbankingplatform/service/UserService.java
@@ -12,13 +12,11 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User register(User user) {
-
         if(userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new RuntimeException("Email already registered");
         }
         return userRepository.save(user);
     }
-
     public User getUserById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
