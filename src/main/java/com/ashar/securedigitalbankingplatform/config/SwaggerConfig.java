3aedf14c932aec5dd2e67ba5f7f0113facbedf67
asharxh <asharxheikh47@gmail.com>
2026-05-16 20:38:31 +0530

    jwt support in swagger

--- src/main/java/com/ashar/securedigitalbankingplatform/config/SwaggerConfig.java
+++ src/main/java/com/ashar/securedigitalbankingplatform/config/SwaggerConfig.java
@@ -4,13 +4,28 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI bankingOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .info(new Info()
                         .title("Secure Digital Banking API")
                         .version("1.0")
