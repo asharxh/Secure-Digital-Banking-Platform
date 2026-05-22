21fd7eb6ccec1cddf2e0a2a4d5056db822d6aba3
asharxh <asharxheikh47@gmail.com>
2026-05-22 21:20:05 +0530

    Integration Testing

---
+++ src/test/java/com/ashar/securedigitalbankingplatform/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,80 @@
+package com.ashar.securedigitalbankingplatform.controller;
+
+import com.ashar.securedigitalbankingplatform.dto.LoginRequestDTO;
+import com.ashar.securedigitalbankingplatform.dto.UserRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String generateEmail() {
+        return "testuser_" + UUID.randomUUID() + "@gmail.com";
+    }
+
+    @Test
+    void shouldRegisterUser() throws Exception {
+
+        String email = generateEmail();
+
+        UserRequestDTO request = new UserRequestDTO();
+        request.setName("Test User");
+        request.setEmail(email);
+        request.setPassword("123456");
+
+        mockMvc.perform(
+                        post("/users/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldLoginSuccessfully() throws Exception {
+
+        String email = generateEmail();
+
+        // FIRST register user
+        UserRequestDTO register = new UserRequestDTO();
+        register.setName("Test User");
+        register.setEmail(email);
+        register.setPassword("123456");
+
+        mockMvc.perform(
+                        post("/users/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(register))
+                )
+                .andExpect(status().isOk());
+
+        // THEN login with same user
+        LoginRequestDTO login = new LoginRequestDTO();
+        login.setEmail(email);
+        login.setPassword("123456");
+
+        mockMvc.perform(
+                        post("/users/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(login))
+                )
+                .andExpect(status().isOk());
+    }
+}
